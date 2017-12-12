08561b948407077d7d2c4a0a56b3744563d21a94
Sarah Ann Garcia <garciatres@gmail.com>
2017-12-11 23:57:02 -0500

    blogpost mongoose app initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.PORT = process.env.PORT || 8080;
---
+++ models.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// this is our schema to represent a restaurant
+const blogPostsSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  created: {type: String}
+});
+
+// *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
+// allow us to define properties on our object that manipulate
+// properties that are stored in the database. Here we use it
+// to generate a human readable string based on the address object
+// we're storing in Mongo.
+blogPostsSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+// this is an *instance method* which will be available on all instances
+// of the model. This method will be used to return an object that only
+// exposes *some* of the fields we want from the underlying data
+blogPostsSchema.methods.serialize = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName,
+    created: this.created,
+  };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const BlogPosts = mongoose.model('BlogPosts', blogPostsSchema);
+
+module.exports = {BlogPosts};
